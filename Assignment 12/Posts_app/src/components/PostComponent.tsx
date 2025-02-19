2f3c58eb3f8542d79bd7a3d46a89d2373b9e432a
Sum <rajamsumit2003@gmail.com>
2025-02-20 03:20:16 +0530

    Post request implemented successfully

--- Assignment 12/Posts_app/src/components/PostComponent.tsx
+++ Assignment 12/Posts_app/src/components/PostComponent.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 type PostType = {
     userId: number;
-    id: number;
+    id?: number;
     title: string;
     body: string;
 };
@@ -12,6 +13,12 @@ export default function FetchPost() {
     const [postId, setPostId] = useState<string>("");
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
+    const [isAdding, setIsAdding] = useState<boolean>(false);
+    const [formData, setFormData] = useState<PostType>({
+        userId: 1,
+        title: "",
+        body: ""
+    });
 
     const fetchPost = async () => {
         if (!postId) return;
@@ -34,6 +41,30 @@ export default function FetchPost() {
             setLoading(false);
         }
     };
+
+    function setNewPostData<T>(key: string, value: T): void {
+        setFormData((prev) => ({ ...prev, [key]: value }));
+    }
+
+    async function createPost(e: React.FormEvent) {
+        e.preventDefault();
+        try {
+            const res = await axios.post(
+                `https://jsonplaceholder.typicode.com/posts`, {
+                title: formData.title,
+                body: formData.body,
+                userId: 1,
+            }
+            );
+            window.alert(`Status: ${JSON.stringify(res.status)}\nResponse Data:${JSON.stringify(res.data)}`);
+
+        } catch (error) {
+            alert(error as Error);
+        } finally {
+            setIsAdding(false);
+        }
+    }
+
     useEffect(() => {
         const timeoutId = setTimeout(() => {
             fetchPost();
@@ -46,10 +77,7 @@ export default function FetchPost() {
         <>
             <div className="container d-flex flex-column mt-5 mb-5 gap-4 shadow-lg p-2 rounded-lg" style={{ width: "330px" }}>
                 <h2>Fetch Post - {postId}</h2>
-                <input
-                    type="number"
-                    placeholder="Enter a post ID"
-                    value={postId}
+                <input type="number" placeholder="Enter a post ID" value={postId}
                     onChange={(e) => {
                         setPostId(e.target.value);
                         setPost(null);
@@ -58,10 +86,6 @@ export default function FetchPost() {
                 {loading && <p>Loading</p>}
                 {error && <p>{error}</p>}
                 {post && postId && (
-                    // <div>
-                    //     <p>{post.title}</p>
-                    // <p>{post.body}</p>
-                    // </div>
                     <div className="card text-center m-3">
                         <div className="card-header">
                             Post No. {post.id}
@@ -73,8 +97,33 @@ export default function FetchPost() {
                     </div>
                 )}
 
-                <button className="btn btn-primary p-3" type="submit">Create New Post</button>
+                <button className="btn btn-primary p-3" type="submit" onClick={() => setIsAdding(true)}>Create New Post</button>
             </div>
+
+            {isAdding && (
+                <div
+                    className="position-fixed top-0 w-100 h-100 d-flex justify-content-center align-items-center"
+                    style={{ zIndex: 50 }}
+                >
+                    <div className="bg-white p-4 rounded shadow-lg" style={{ width: "320px" }}>
+                        <h5 className="text-center">Add New Task</h5>
+                        <form>
+                            <label className="form-label">Title</label>
+                            <input type="text" className="form-control" placeholder="Enter title" onChange={(e) => { setNewPostData('title', e.target.value) }} required />
+                            <label className="form-label">Body</label>
+                            <textarea className="form-control" rows={3} placeholder="Enter description" onChange={(e) => { setNewPostData('body', e.target.value) }} required></textarea>
+                            <div className="d-flex mt-3">
+                                <button type="submit" className="btn btn-primary w-50 me-2" onClick={createPost}>
+                                    Save
+                                </button>
+                                <button type="button" className="btn btn-secondary w-50" onClick={() => setIsAdding(false)}>
+                                    Cancel
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
         </>
     );
 };
